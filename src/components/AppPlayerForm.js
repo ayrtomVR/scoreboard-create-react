e4eb84da00fbc043903e915e2071e3bb199815f8
Tommy Vasquez <tommyvasquezr@gmail.com>
2020-08-06 10:35:42 -0500

    refs and the DOM

--- src/components/AppPlayerForm.js
+++ src/components/AppPlayerForm.js
@@ -1,5 +1,24 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React from 'react';
 
+const AddPlayerForm = ({ addPlayer }) => {
+    let playerInput = React.createRef();
+    let handleSubmit = (e) => {
+        e.preventDefault();
+        addPlayer(playerInput.current.value);
+        e.currentTarget.reset();
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" ref={playerInput} placeholder="Enter a player's name" />
+
+            <input type="submit" value="Add Player" />
+        </form>
+    );
+};
+
+/*
 class AddPlayerForm extends Component {
     state = {
         value: '',
@@ -29,5 +48,6 @@ class AddPlayerForm extends Component {
         );
     }
 }
+*/
 
 export default AddPlayerForm;
